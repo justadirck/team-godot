a6ec42c0f61f9d918ebe0116f78d8ef6f25c674f
Dirck Hecking <dhecking@gmail.com>
2019-10-16 14:24:05 -0400

    jumpin

--- src/actors/Actor.gd
+++ src/actors/Actor.gd
@@ -6,7 +6,7 @@ const FLOOR_NORMAL: = Vector2.UP
 export var speed = Vector2(300.0, 1000.0)
 export var gravity = 3000.0
 
-var velocity: = Vector2.ZERO
+var velocity: Vector2 = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
